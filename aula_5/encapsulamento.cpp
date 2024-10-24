d8d12b4d5a01414a5ff3179ebd2c2d2ea8d84c15
Daniel Francisco Braga de Araújo <20232014040002@ifrn.local>
2024-10-24 08:23:00 -0300

    area do circulo

--- aula_5/encapsulamento.cpp
+++ aula_5/encapsulamento.cpp
@@ -26,23 +26,83 @@ class triangulo{
         }
 };
 
+class circulo{
+    private:
+        double raio;
+    public:
+        void setRaio(double v){
+            if(v > 0){
+                raio = v;
+            }
+        }
+        double getRaio(){
+            return raio;
+        }
+        double calcCirculo(){
+            return (raio * raio) * 3.14;
+        }
+};
+
+class UI{
+    public:
+    void prin(){
+        int op = menu();
+        while (op != 0){
+            if (op == 1){
+                double area = novoTriangulo();
+                std::cout << "A area do triangulo eh: " << area << "\n";
+            }
+            if (op == 2){
+                double areaC = novoCirculo();
+                std::cout << "A area do circulo eh: " << areaC << "\n";
+            }
+            op = menu();
+        }
+    }
+    int menu(){
+        int op = 0;
+        std::cout << "1-Triangulo 2-Circulo 0-Sair\n";
+        std::cin >> op;
+        return op;
+    }
+    double novoTriangulo(){
+        triangulo x;
+        double base;
+        double altura;
+
+        std::cout << "Digite a base do triangulo: ";
+        std::cin >> base;
+
+        std::cout << "Digite a altura do triangulo: ";
+        std::cin >> altura;
+
+        x.setBase(base);
+        x.setAltura(altura);
+
+        double area = x.calcArea();
+
+        return area;
+    }
+
+    double novoCirculo(){
+        circulo y;
+        double raio;
+        
+        std::cout << "Digite o raio do circulo: ";
+        std::cin >> raio;
+
+        y.setRaio(raio);
+
+        double areaC = y.calcCirculo();
+
+        return areaC;
+    }
+};
+
 int main(){
-    triangulo x;
-    double base;
-    double altura;
+    UI x;
 
-    std::cout << "Digite a base do triangulo: ";
-    std::cin >> base;
-
-    std::cout << "Digite a altura do triangulo: ";
-    std::cin >> altura;
-
-    x.setBase(base);
-    x.setAltura(altura);
-
-    double area = x.calcArea();
-
-    std::cout << area << "\n";
+    x.prin();
 
     return 0;
 }
