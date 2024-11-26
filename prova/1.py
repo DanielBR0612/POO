4dca186d5e39724df4fed0bd75d8ace5312bf020
Daniel Braga <danielfranciscobragadearaujo@gmail.com>
2024-11-26 18:39:49 -0300

    prova update

--- prova/1.py
+++ prova/1.py
@@ -1,95 +1,90 @@
-from datetime import datetime
-
 class Item:
-    def __init__(self, produto, qtd, preco):
-        self.__produto = produto
+    def __init__(self, prod, qtd, preco):
+        self.__produto = prod
         self.__qtd = qtd
         self.__preco = preco
-
+    
     @property
     def produto(self):
         return self.__produto
-
+    
     @produto.setter
-    def produto(self, valor):
-        if valor != "":
-            self.__produto = valor
+    def produto(self, p):
+        if p != "":
+            self.__produto = p
         else:
-            raise ValueError("Digite um produto válido")
-
+            raise ValueError("Digite um produto valido")
+        
     @property
     def qtd(self):
         return self.__qtd
-
+    
     @qtd.setter
-    def qtd(self, valor):
-        if valor >= 0:
-            self.__qtd = valor
+    def qtd(self, qtd):
+        if qtd >= 0:
+            self.__qtd = qtd
         else:
-            raise ValueError("Digite uma quantidade válida")
+            raise ValueError("Digite um produto valido")
 
     @property
     def preco(self):
         return self.__preco
-
+    
     @preco.setter
-    def preco(self, valor):
-        if valor >= 0:
-            self.__preco = valor
+    def produto(self, preco):
+        if preco >= 0:
+            self.__preco = preco
         else:
-            raise ValueError("Digite um preço válido")
-
+            raise ValueError("Digite um produto valido")
+    
     def total(self):
-        return self.__qtd * self.__preco
-
+        return self.__preco * self.__qtd
+    
     def __str__(self):
-        return f"Produto: {self.__produto} - Quantidade: {self.__qtd} - Preço: {self.__preco:.2f}"
-
-
+        return f"Produto: {self.__produto} - Quantidade: {self.__qtd} - Preco: {self.__preco}"
+    
 class Carrinho:
-    def __init__(self, cliente, data):
-        self.__cliente = cliente
-        self.__data = data
+    def __init__(self, c, d):
+        self.__cliente = c
+        self.__data = d
         self.__itens = []
-
+    
     @property
     def cliente(self):
         return self.__cliente
 
     @cliente.setter
-    def cliente(self, valor):
-        if valor != "":
-            self.__cliente = valor
+    def cliente(self, c):
+        if c != "":
+            self.__cliente = c
         else:
-            raise ValueError("Digite um cliente válido")
-
-    @property
+            raise ValueError("Digite um cliente valido")
+    
+    @property 
     def data(self):
         return self.__data
 
     @data.setter
-    def data(self, valor):
-        self.__data = valor
-
+    def data(self, d):
+        self.__data = d
+    
     def inserir(self, item):
         self.__itens.append(item)
-
+    
     def remover(self, item):
         if item in self.__itens:
             self.__itens.remove(item)
-
-    def total(self):
-        return sum(item.total() for item in self.__itens)
-
+    
     def listar(self):
         for item in self.__itens:
             print(item)
 
+    def total(self):
+        return sum(item.total() for item in self.__itens)
+
     def __str__(self):
         return f"Cliente: {self.__cliente}, Data: {self.__data}, Número de Itens: {len(self.__itens)}, Total do Carrinho: {self.total():.2f}"
 
-
-# Criando objetos Item
 item1 = Item("Notebook", 1, 3000.0)
 item2 = Item("Mouse", 2, 50.0)
 item3 = Item("Teclado", 1, 150.0)
