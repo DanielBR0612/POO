b070abd5b7a544487ec0a93ce7d8ee17c18946ac
Daniel Braga <101554437+DanielBR0612@users.noreply.github.com>
2024-12-12 21:46:10 +0000

    categoria update

--- Projeto/templates/mantercategoriaUI.py
+++ Projeto/templates/mantercategoriaUI.py
@@ -13,48 +13,46 @@ class ManterCategoriaUI:
         with tab4: ManterCategoriaUI.excluir()
 
     def listar():
-        clientes = View.categoria_listar()
+        Categorias = View.categoria_listar()
         if len(Categorias) == 0: 
             st.write("Nenhuma categoria cadastrado")
         else:    
-            #for obj in clientes: st.write(obj)
+            
             dic = []
-            for obj in categorias: dic.append(obj.__dict__)
+            for obj in Categorias: dic.append(obj.__dict__)
             df = pd.DataFrame(dic)
             st.dataframe(df)
 
     def inserir():
-        categoria_id = st.text_input("Informe a descrição: ")
-        descricao = st.text_input("Informe o preço: ")
+        descricao = st.text_input("Informe a descrição: ")
 
         if st.button("Inserir"):
-            View.categoria_inserir(categoria_id, descricao)
+            View.categoria_inserir(descricao)
             st.success("Produto inserido com sucesso")
             time.sleep(2)
             st.rerun()
 
     def atualizar():
         categoria = View.categoria_listar()
-        if len(clientes) == 0: 
+        if len(categoria) == 0: 
             st.write("Nenhum produto cadastrado")
         else:
-            op = st.text_input("Atualização de categoria", Categorias)
-            categoria_id = st.text_input("Informe o novo categoria_id do cliente", op.categoria_id)
-            descricao = st.text_input("Informe o novo e-mail", op.descricao)
+            op = st.selectbox("Atualização de categoria", categoria)
+            descricao = st.text_input("Informe a nova descricao", op.descricao)
             if st.button("Atualizar"):
-                View.categoria_atualizar(op.categoria_id, categoria_id)
+                View.categoria_atualizar(op.id, descricao)
                 st.success("Categoria atualizado com sucesso")
                 time.sleep(2)
                 st.rerun()
 
     def excluir():
-        Produtos = View.produto_listar()
-        if len(Produtos) == 0: 
+        Categorias = View.categoria_listar()
+        if len(Categorias) == 0: 
             st.write("Nenhuma categoria cadastrado")
         else:
             op = st.selectbox("Exclusão de categoria", Categorias)
             if st.button("Excluir"):
-                View.categoria_excluir(op.categoria_id)
+                View.categoria_excluir(op.id)
                 st.success("Categoria excluído com sucesso")
                 time.sleep(2)
                 st.rerun()
